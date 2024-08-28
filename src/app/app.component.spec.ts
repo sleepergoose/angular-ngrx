f7a011c228a43893cf4dd504ef7a4f1780503234
sleepergoose <sleepergoose@gmail.com>
2024-08-28 16:11:13 +0300

    Remove karma-jasmine, add jest, add eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-ngrx' title`, () => {
+  it("should have the 'angular-ngrx' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-ngrx');
